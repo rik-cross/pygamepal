fd27409083546272ceacfd8183df87a75f5c17a4
rik-cross <rik@raspberrypi.org>
2023-08-27 11:13:27 +0100

    Game template

---
+++ gameTemplate.py
@@ -0,0 +1,26 @@
+import pygame_utils
+
+#
+# create a new game
+#
+
+class MyGame(pygame_utils.Game):
+
+    def init(self):
+        # replace 'pass' below with your code
+        pass
+
+    def update(self):
+        # replace 'pass' below with your code
+        pass
+
+    def draw(self):
+        # replace 'pass' below with your code
+        pass
+
+#
+# create a new game instance
+#
+
+myGame = MyGame()
+myGame.run()
