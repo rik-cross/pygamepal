f7f9d73d9bf2aa2c6b2448cbc687eda1992a8c79
Rik Cross <rik@raspberrypi.org>
2024-04-17 10:37:19 +0100

    Cached default font

--- src/pygamepal/drawText.py
+++ src/pygamepal/drawText.py
@@ -1,9 +1,15 @@
 import pygame
 
+# create a default 'system' font
+pygame.font.init()
+sysFont = pygame.font.SysFont(None, 24)
+
 def drawText(screen, text, x = 0, y = 0, font = None, antialias = True, color = (255, 255, 255), background = None):
-    # use 'standard' font if none specified
+    
+    # use the default 'system' font if none specified
     if font is None:
-        font = pygame.font.SysFont(None, 24)
+        font = sysFont
+
     # create text surface
     textSurface = font.render(text, antialias, color, background)
     # draw text to screen
