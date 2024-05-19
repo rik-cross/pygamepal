373f0e848b8ee4fb51c8a6c1e782fed66d4243a7
Rik Cross <rik@raspberrypi.org>
2024-05-19 18:20:16 +0100

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
@@ -20,7 +19,7 @@ release = '0.6.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
-              'sphinx_rtd_theme',]
+              'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
