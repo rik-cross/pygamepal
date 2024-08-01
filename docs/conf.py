8d4ca402fe2b067596b97255490e747d3097b7f3
Rik Cross <rik@raspberrypi.org>
2024-08-01 17:33:14 +0100

    v0.7

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 project = 'PygamePal'
 copyright = '2024, Rik Cross'
 author = 'Rik Cross'
-release = '0.6.3'
+release = '0.7.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
